469e40a97e32c925dfcecc21947fea7668277e15
jcn33 <jnastas9@gmail.com>
2022-01-24 16:10:58 -0500

    Fixed modularity for dosing function

--- dosing.py
+++ dosing.py
@@ -3,17 +3,14 @@
 #Readable
 #Testable
 
-
-
 """dosing.py
     Example program of calculating first-day dose of medicine for pediatric
         patients.
     NOTE:  This is a programming example, and should not be used for any
              type of medical treatment or diagnostics.
 """
-
-
-def dose_amount():
+          
+def diagnosis_type():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -21,6 +18,52 @@ def dose_amount():
     print("2 - Acute bacterial sinusitis")
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
+    diagnosis = int(input("Enter a number: "))
+    return diagnosis
+    
+def patient_info():
+    print("PATIENT WEIGHT")
+    print("Enter patient weight followed by units of kg or lb.")
+    print("Examples:  65.3 lb      21.0 kg")
+    weight_input = input("Enter weight: ")
+    weight_data = weight_input.split(" ")
+    weight = float(weight_data[0])
+    units = weight_data[1]
+    if units == "lb":
+        weight = weight / 2.205
+    return weight
+    
+def dosage_calc(weight, diagnosis):
+    dosages_mg_per_kg = [30, 10, 10, 12]
+    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+    dosage_mg_first_day = weight * dosage_mg_per_kg
+    print("CORRECT DOSAGE")
+    print("For a patient weighing {:.1f} kg,".format(weight))
+    print("  the correct dosage is {:.1f} mg the first day".format(dosage_mg_first_day))
+
+def dose_amount():
+    
+    # Initial choosing of diag. type
+    diagnosis = diagnosis_type()
+
+    #Enter patient weight info
+    weight = patient_info()
+    
+    #Dosage output
+    dosage_calc(weight, diagnosis)
+
+def old_dose_amount():
+
+    # Initial choosing of diag. type
+    print("Day One Dosing Guidelines")
+    print("")
+    print("Choose diagnosis:")
+    print("1 - Acute otitis media")
+    print("2 - Acute bacterial sinusitis")
+    print("3 - Community-acquired pneumonia")
+    print("4 - Pharyngitis/tonsilitis")
+    
+    #Enter patient weight info
     diagnosis = int(input("Enter a number: "))
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
@@ -31,6 +74,8 @@ def dose_amount():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+        
+    #Dosage output
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
@@ -38,7 +83,6 @@ def dose_amount():
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
-
 
 if __name__ == '__main__':
     dose_amount()
